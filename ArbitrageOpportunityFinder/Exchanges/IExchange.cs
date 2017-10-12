b4c5ae2c201148da555bcd3dcd8a2ec042f4008e
jameshackl <james.f.hackl@gmail.com>
2017-10-11 20:33:13 -0600

    start of an entity approach instead of a parser approach

---
+++ ArbitrageOpportunityFinder/Exchanges/IExchange.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArbitrageOpportunityFinder.Exchanges
+{
+    interface IExchange
+    {
+        GlobalData.Exchange exchange { get; set; }
+        bool marginTradingAvailable { get; set; }
+        Currency marginTradingBase { get; set; }
+
+        decimal makerFee { get; set; }
+        decimal takerFee { get; set; }
+
+
+    }
+}
