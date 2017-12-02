b3d352c83151815ab37fff57aa2636863756bf58
jameshackl <james.f.hackl@gmail.com>
2017-12-01 21:57:55 -0700

    splash screen to look at while it collects data

---
+++ ArbitrageOpportunityFinder/InitializationSplash.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ArbitrageOpportunityFinder
+{
+    /// <summary>
+    /// Interaction logic for InitializationSplash.xaml
+    /// </summary>
+    public partial class InitializationSplash : Window
+    {
+        public InitializationSplash()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        public void OnInit(object sender, RoutedEventArgs e)
+        {
+            DataController.GetInitData();
+            DataController.GetTransactionData();
+
+            GlobalData db = GlobalData.Instance;
+            db.GenerateTriArbTransactionList();
+
+            this.Close();
+        }
+    }
+}
