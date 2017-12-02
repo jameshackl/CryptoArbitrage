5fdf9c1df8a9d50b3c82952315235d9a2f8d9e1d
jameshackl <james.f.hackl@gmail.com>
2017-12-01 21:59:26 -0700

    new view model

---
+++ ArbitrageOpportunityFinder/TriArbMasterViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArbitrageOpportunityFinder
+{
+    public class TriArbMasterViewModel
+    {
+
+        public ObservableCollection<TriArbViewModel> Items { get; set; }
+
+        public TriArbMasterViewModel()
+        {
+            GlobalData db = GlobalData.Instance;
+            Items = db.GetTriArbOutput();
+        }
+    }
+}
